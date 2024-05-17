61e8214729f39144bb0b5315072c718929d399ad
matiudev <garcia90mati@gmail.com>
2024-05-17 18:08:46 -0400

    responsive

--- scripts.js
+++ scripts.js
@@ -12,7 +12,8 @@ const peliculasTendencia = [
         titulo: "Mundos Paralelos",
         año: 2022,
         imagen: "peliculas/drama/mundos_paralelos/mundos-paralelos.webp",
-        imagen_horizontal: "peliculas/drama/mundos_paralelos/mundos-paralelos-horizontal.webp",
+        imagen_horizontal:
+            "peliculas/drama/mundos_paralelos/mundos-paralelos-horizontal.webp",
         genero: "Drama",
         descripcion:
             "Las vidas de cuatro antiguos amigos se ven alteradas cuando un misterioso suceso los separa, enviándolos a dimensiones paralelas. Intentan comprender lo que ha sucedido y se esfuerzan por hacer retroceder el tiempo, para volver a su realidad.",
@@ -21,7 +22,8 @@ const peliculasTendencia = [
         titulo: "Titanic",
         año: 1997,
         imagen: "peliculas/romance/titanic/titanic.jpg",
-        imagen_horizontal: "https://www.100hdwallpapers.com/wallpapers/1920x1080/titanic_movie-hd_wallpapers.jpg",
+        imagen_horizontal:
+            "https://www.100hdwallpapers.com/wallpapers/1920x1080/titanic_movie-hd_wallpapers.jpg",
         genero: "Romance",
         descripcion:
             "Una joven de la alta sociedad abandona a su arrogante pretendiente por un artista humilde en el trasatlántico que se hundió durante su viaje inaugural.",
@@ -48,7 +50,8 @@ const peliculasTendencia = [
         titulo: "Orgullo y prejuicio",
         año: 2005,
         imagen: "peliculas/romance/orgullo-y-prejuicio/orgullo-y-prejuicio.jpg",
-        imagen_horizontal: "https://discordiamag.com/wp-content/uploads/2021/02/Portada-e1676814355157.jpg",
+        imagen_horizontal:
+            "https://discordiamag.com/wp-content/uploads/2021/02/Portada-e1676814355157.jpg",
         genero: "Romance",
         descripcion:
             "Elizabeth Bennet conoce al apuesto y adinerado Sr. Darcy, con quien, rápidamente, inicia una intensa y compleja relación.",
@@ -60,41 +63,52 @@ const peliculasRecienEstrenadas = [
         titulo: "Godzilla y Kong: El nuevo imperio",
         año: 2024,
         imagen: "peliculas/ciencia-ficcion/gozdilla-y-kong/gozdilla-y-kong.jpg",
-        imagen_horizontal: "https://e.rpp-noticias.io/xlarge/2024/03/18/573157_1554528.webp",
+        imagen_horizontal:
+            "https://e.rpp-noticias.io/xlarge/2024/03/18/573157_1554528.webp",
         genero: "Ciencia ficción",
-        descripcion: "Godzilla y Kong se enfrentan a una amenaza colosal escondida en lo profundo del planeta, desafiando su propia existencia y la supervivencia de la raza humana."
+        descripcion:
+            "Godzilla y Kong se enfrentan a una amenaza colosal escondida en lo profundo del planeta, desafiando su propia existencia y la supervivencia de la raza humana.",
     },
     {
         titulo: "Abigail",
         año: 2024,
         imagen: "https://pics.filmaffinity.com/Abigail-133797089-large.jpg",
-        imagen_horizontal: "https://m.media-amazon.com/images/M/MV5BMGNmNWY2ZjUtYjE4My00MWY0LTliMzItZTI4ODA2MzU2YjFmXkEyXkFqcGdeQWFybm8@._V1_.jpg",
+        imagen_horizontal:
+            "https://m.media-amazon.com/images/M/MV5BMGNmNWY2ZjUtYjE4My00MWY0LTliMzItZTI4ODA2MzU2YjFmXkEyXkFqcGdeQWFybm8@._V1_.jpg",
         genero: "Terror",
-        descripcion: "Abigail es una película de terror estadounidense dirigida por Matt Bettinelli-Olpin y Tyler Gillett a partir de un guion escrito por Stephen Shields y Guy Busick."
+        descripcion:
+            "Abigail es una película de terror estadounidense dirigida por Matt Bettinelli-Olpin y Tyler Gillett a partir de un guion escrito por Stephen Shields y Guy Busick.",
     },
     {
         titulo: "Garfield: fuera de casa",
         año: 2024,
-        imagen:"peliculas/infantil/garfield-fuera-de-casa/garfield-fuera-de-casa.webp",
-        imagen_horizontal: "https://imagenes.heraldo.es/files/image_990_556/uploads/imagenes/2023/11/14/garfield-pelicula-2024-trailer-estreno.jpeg",
+        imagen:
+            "peliculas/infantil/garfield-fuera-de-casa/garfield-fuera-de-casa.webp",
+        imagen_horizontal:
+            "https://imagenes.heraldo.es/files/image_990_556/uploads/imagenes/2023/11/14/garfield-pelicula-2024-trailer-estreno.jpeg",
         genero: "Infantil",
-        descripcion: "El mundialmente famoso Garfield, el gato casero que odia los lunes y que adora la lasaña, está a punto de vivir una aventura ¡en el salvaje mundo exterior! Tras una inesperada reunión con su largamente perdido padre."
+        descripcion:
+            "El mundialmente famoso Garfield, el gato casero que odia los lunes y que adora la lasaña, está a punto de vivir una aventura ¡en el salvaje mundo exterior! Tras una inesperada reunión con su largamente perdido padre.",
     },
     {
         titulo: "Kung Fu Panda 4",
         año: 2024,
         imagen: "peliculas/infantil/kung-fu-panda-4/kung-fu-panda-4.jpg",
-        imagen_horizontal: "https://rubik-audiovisual.com/wp-content/uploads/2024/03/kung-fu-panda-4-scaled.jpg",
+        imagen_horizontal:
+            "https://rubik-audiovisual.com/wp-content/uploads/2024/03/kung-fu-panda-4-scaled.jpg",
         genero: "Infantil",
-        descripcion: "Po está entrenando a un nuevo guerrero que ocupe su lugar para que él pueda convertirse en líder espiritual del Valle de la Paz. Sin embargo, la irrupción de una malvada hechicera que puede cambiar de apariencia trastoca sus planes."
+        descripcion:
+            "Po está entrenando a un nuevo guerrero que ocupe su lugar para que él pueda convertirse en líder espiritual del Valle de la Paz. Sin embargo, la irrupción de una malvada hechicera que puede cambiar de apariencia trastoca sus planes.",
     },
     {
         titulo: "Despicable Me 4",
         año: 2024,
         imagen: "peliculas/infantil/despicable-me-4/despicable-me-4.jpg",
-        imagen_horizontal: "https://cooperativa.cl/noticias/site/artic/20240129/imag/foto_0000000220240129093758.jpg",
+        imagen_horizontal:
+            "https://cooperativa.cl/noticias/site/artic/20240129/imag/foto_0000000220240129093758.jpg",
         genero: "Infantil",
-        descripcion: "Despicable Me 4 es una próxima película de comedia animada estadounidense producida por Illumination y distribuida por Universal Pictures.",
+        descripcion:
+            "Despicable Me 4 es una próxima película de comedia animada estadounidense producida por Illumination y distribuida por Universal Pictures.",
     },
 ];
 
@@ -105,7 +119,8 @@ const seriesPopulares = [
         imagen: "series/suspenso/murder/murder.jpg",
         imagen_horizontal: "series/suspenso/murder/murder-horizontal.webp",
         genero: "Suspenso",
-        descripcion: "Annalise Keating es una brillante y carismática profesora de derecho penal, que inesperadamente, tiene que aplicar todo el conocimiento práctico junto con sus estudiantes cuando se ven involucrados en un complot de asesinato en la universidad.",
+        descripcion:
+            "Annalise Keating es una brillante y carismática profesora de derecho penal, que inesperadamente, tiene que aplicar todo el conocimiento práctico junto con sus estudiantes cuando se ven involucrados en un complot de asesinato en la universidad.",
     },
     {
         titulo: "La ley de los Audaces",
@@ -113,7 +128,8 @@ const seriesPopulares = [
         imagen: "series/drama-legal/suits/433386.webp",
         imagen_horizontal: "series/drama-legal/suits/gettyimages-880283214.webp",
         genero: "Drama Legal",
-        descripcion: "El gran abogado corporativo de Manhattan, Harvey Specter y su equipo, se lanzan a un juego por obtener el poder cuando un nuevo socio se une a la empresa. Con sus dos mejores colegas fuera y Jessica de regreso en Chicago, Specter y el equipo intentan adaptarse a una nueva situación sin ellos.",
+        descripcion:
+            "El gran abogado corporativo de Manhattan, Harvey Specter y su equipo, se lanzan a un juego por obtener el poder cuando un nuevo socio se une a la empresa. Con sus dos mejores colegas fuera y Jessica de regreso en Chicago, Specter y el equipo intentan adaptarse a una nueva situación sin ellos.",
     },
     {
         titulo: "Stranger Things",
@@ -121,35 +137,38 @@ const seriesPopulares = [
         imagen: "series/terror/stranger-things/5638861.webp",
         imagen_horizontal: "series/terror/stranger-things/Stranger_Things_logo.png",
         genero: "Terror",
-        descripcion: "Cuando un niño desaparece, sus amigos, la familia y la policía se ven envueltos en una serie de eventos misteriosos al tratar de encontrarlo. Su ausencia coincide con el avistamiento de una criatura terrorífica y la aparición de una extraña niña.",
+        descripcion:
+            "Cuando un niño desaparece, sus amigos, la familia y la policía se ven envueltos en una serie de eventos misteriosos al tratar de encontrarlo. Su ausencia coincide con el avistamiento de una criatura terrorífica y la aparición de una extraña niña.",
     },
     {
         titulo: "Cobra Kai",
         año: 2018,
-        imagen: "series/drama/cobra-kai/MV5BYWU4ZmI0NTItZjcyNy00MzQ5LThiNDQtZDZkNjg1NWUwN2RhXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_.jpg",
+        imagen:
+            "series/drama/cobra-kai/MV5BYWU4ZmI0NTItZjcyNy00MzQ5LThiNDQtZDZkNjg1NWUwN2RhXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_.jpg",
         imagen_horizontal: "series/drama/cobra-kai/16102988786234.webp",
         genero: "Drama",
-        descripcion: "Treinta y cuatro años después de los eventos del torneo de karate All Valley de 1984, Johnny Lawrence busca la redención al abrir un dojo Cobra Kai, reavivando su rivalidad con un exitoso Daniel LaRusso.",
+        descripcion:
+            "Treinta y cuatro años después de los eventos del torneo de karate All Valley de 1984, Johnny Lawrence busca la redención al abrir un dojo Cobra Kai, reavivando su rivalidad con un exitoso Daniel LaRusso.",
     },
-]
+];
 
 function crearTarjetaPelicula(pelicula) {
     const card = document.createElement("div");
     card.classList.add("card");
-  
+
     const imagen = document.createElement("img");
     imagen.src = pelicula.imagen;
     imagen.alt = pelicula.titulo;
-  
+
     const contenido = document.createElement("div");
     contenido.classList.add("card-content");
     contenido.textContent = pelicula.titulo;
-  
+
     const detalles = document.createElement("div");
     detalles.classList.add("movie-details");
 
     let timeoutId;
-  
+
     const detallesImagen = document.createElement("img");
     detallesImagen.src = pelicula.imagen_horizontal;
     detallesImagen.alt = pelicula.titulo;
@@ -162,59 +181,70 @@ function crearTarjetaPelicula(pelicula) {
     detallesImagen.style.zIndex = "-1";
     detallesImagen.style.overflow = "hidden%";
     detallesImagen.style.maskImage = "linear-gradient(black 5%,transparent)";
-  
+
     const detallesTitulo = document.createElement("h3");
     detallesTitulo.textContent = pelicula.titulo;
     detallesTitulo.style.marginTop = "110px";
     detallesTitulo.style.padding = "0 20px";
-    
-  
+
     const detallesBoton = document.createElement("button");
     detallesBoton.textContent = "Ver ahora";
-    detallesBoton.style.fontSize = "unset"
+    detallesBoton.style.fontSize = "unset";
     detallesBoton.style.fontWeight = "bold";
     detallesBoton.style.marginBottom = "10px";
     detallesBoton.style.marginLeft = "20px";
     detallesBoton.style.padding = "10px 70px";
     detallesBoton.style.borderRadius = "30px";
     detallesBoton.style.borderStyle = "none";
-  
+
     const detallesGenero = document.createElement("p");
     detallesGenero.textContent = `Género: ${pelicula.genero}`;
     detallesGenero.style.marginBottom = "10px";
     detallesGenero.style.padding = "0 20px";
-  
+
     const detallesDescripcion = document.createElement("p");
     detallesDescripcion.textContent = pelicula.descripcion;
     detallesDescripcion.style.padding = "0 20px";
-  
+
     detalles.appendChild(detallesImagen);
     detalles.appendChild(detallesTitulo);
     detalles.appendChild(detallesBoton);
     detalles.appendChild(detallesGenero);
     detalles.appendChild(detallesDescripcion);
-  
+
     card.appendChild(imagen);
     card.appendChild(contenido);
     card.appendChild(detalles);
-  
-    card.addEventListener("mouseover", () => {
-        // Elimina cualquier temporizador pendiente
+
+
+
+    function mostrarTarjeta() {
         clearTimeout(timeoutId);
-    
-        // Establece un nuevo temporizador de 1 segundo (1000 milisegundos)
         timeoutId = setTimeout(() => {
-          detalles.classList.add("show");
+            detalles.style.opacity = '1'; // Establecer la opacidad a 1
+            detalles.classList.add("show"); // Agregar la clase 'show' para mostrar los detalles
         }, 500);
-      });
-    
-      card.addEventListener("mouseout", () => {
-        clearTimeout(timeoutId); // Elimina el temporizador
-        detalles.classList.remove("show");
-      });
-    
-      return card;
+        
     }
+    
+    // Función para ocultar la tarjeta
+    function ocultarTarjeta() {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => {
+            detalles.style.opacity = '0'; // Establecer la opacidad a 0
+            detalles.classList.remove("show"); // Quitar la clase 'show' para ocultar los detalles
+        }, 50);
+    }
+    
+    // Agregar listeners de eventos
+    imagen.addEventListener("mouseover", mostrarTarjeta);
+    imagen.addEventListener("mouseout", ocultarTarjeta);
+    
+    detalles.addEventListener("mouseover", mostrarTarjeta);
+    detalles.addEventListener("mouseout", ocultarTarjeta);
+
+    return card;
+}
 
 // Función para mostrar las películas de recién estreno en tarjetas
 function mostrarPeliculasRecienEstrenadas() {
@@ -250,7 +280,6 @@ function mostrarTodasLasPeliculas() {
     });
 }
 
-
 // Llama a las funciones para mostrar las películas cuando la página cargue
 window.onload = () => {
     mostrarTodasLasPeliculas();
@@ -258,16 +287,11 @@ window.onload = () => {
     mostrarSeriesPopulares();
 };
 
-
-
-
-
-let menuList = document.getElementById("menuList")
+let menuList = document.getElementById("menuList");
 menuList.style.maxHeight = "0px";
 
 function toggleMenu() {
-    if (menuList.style.maxHeight == "0px") 
-        {
+    if (menuList.style.maxHeight == "0px") {
         menuList.style.maxHeight = "400px";
     } else {
         menuList.style.maxHeight = "0px";
