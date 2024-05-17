da35cacc158c32802243680aa060b0298953d20d
matiudev <garcia90mati@gmail.com>
2024-05-16 23:26:32 -0400

    first commit

---
+++ scripts.js
@@ -0,0 +1,258 @@
+const peliculasTendencia = [
+    {
+        titulo: "El Padrino",
+        año: 1972,
+        imagen: "peliculas/drama/El_Padrino/el-padrino.jpg",
+        imagen_horizontal: "peliculas/drama/El_Padrino/el-padrino-horizontal.webp",
+        genero: "Drama",
+        descripcion:
+            "La compleja historia de una familia mafiosa liderada por un implacable padre de familia y hombre de honor (inmenso Marlon Brando) y cuyo poder hereda su hijo más pacífico (asombroso un primerizo Al Pacino de mirada gélida) es, ante todo, un ejercicio narrativo apabullante, de insuperable nivel.",
+    },
+    {
+        titulo: "Mundos Paralelos",
+        año: 2022,
+        imagen: "peliculas/drama/mundos_paralelos/mundos-paralelos.webp",
+        imagen_horizontal: "peliculas/drama/mundos_paralelos/mundos-paralelos-horizontal.webp",
+        genero: "Drama",
+        descripcion:
+            "Las vidas de cuatro antiguos amigos se ven alteradas cuando un misterioso suceso los separa, enviándolos a dimensiones paralelas. Intentan comprender lo que ha sucedido y se esfuerzan por hacer retroceder el tiempo, para volver a su realidad.",
+    },
+    {
+        titulo: "Titanic",
+        año: 1997,
+        imagen: "peliculas/romance/titanic/titanic.jpg",
+        imagen_horizontal: "https://www.100hdwallpapers.com/wallpapers/1920x1080/titanic_movie-hd_wallpapers.jpg",
+        genero: "Romance",
+        descripcion:
+            "Una joven de la alta sociedad abandona a su arrogante pretendiente por un artista humilde en el trasatlántico que se hundió durante su viaje inaugural.",
+    },
+    {
+        titulo: "Busqueda Implacable",
+        año: 2008,
+        imagen: "peliculas/accion/taken-1/taken1.jpg",
+        imagen_horizontal: "https://s1.dmcdn.net/v/VSTvk1bQ7BKNUaQ-g/x1080",
+        genero: "Accion",
+        descripcion:
+            "El exagente de las fuerzas especiales de élite Bryan Millis se ve enredado en la trama de una organización criminal cuando intenta salvar a su hija Kim, pero solo tiene 96 horas para rescatarla antes de perder el rastro.",
+    },
+    {
+        titulo: "Busqueda Implacable 2",
+        año: 2012,
+        imagen: "peliculas/accion/taken-2/taken2.jpg",
+        imagen_horizontal: "https://s2.dmcdn.net/v/VSU4h1bQ7BQWj7n87/x1080",
+        genero: "Accion",
+        descripcion:
+            "En Estambul, Bryan y su familia se encuentran con Murad, cuyo hijo murió a manos de Bryan durante el rescate de Kim. Murad quiere vengarse y ahora es Kim quien debe salvar a su padre.",
+    },
+    {
+        titulo: "Orgullo y prejuicio",
+        año: 2005,
+        imagen: "peliculas/romance/orgullo-y-prejuicio/orgullo-y-prejuicio.jpg",
+        imagen_horizontal: "https://discordiamag.com/wp-content/uploads/2021/02/Portada-e1676814355157.jpg",
+        genero: "Romance",
+        descripcion:
+            "Elizabeth Bennet conoce al apuesto y adinerado Sr. Darcy, con quien, rápidamente, inicia una intensa y compleja relación.",
+    },
+];
+
+const peliculasRecienEstrenadas = [
+    {
+        titulo: "Godzilla y Kong: El nuevo imperio",
+        año: 2024,
+        imagen: "peliculas/ciencia-ficcion/gozdilla-y-kong/gozdilla-y-kong.jpg",
+        imagen_horizontal: "https://e.rpp-noticias.io/xlarge/2024/03/18/573157_1554528.webp",
+        genero: "Ciencia ficción",
+        descripcion: "Godzilla y Kong se enfrentan a una amenaza colosal escondida en lo profundo del planeta, desafiando su propia existencia y la supervivencia de la raza humana."
+    },
+    {
+        titulo: "Abigail",
+        año: 2024,
+        imagen: "peliculas/terror/Abigail/Abigail.jpg",
+        imagen_horizontal: "https://m.media-amazon.com/images/M/MV5BMGNmNWY2ZjUtYjE4My00MWY0LTliMzItZTI4ODA2MzU2YjFmXkEyXkFqcGdeQWFybm8@._V1_.jpg",
+        genero: "Terror",
+        descripcion: "Abigail es una película de terror estadounidense dirigida por Matt Bettinelli-Olpin y Tyler Gillett a partir de un guion escrito por Stephen Shields y Guy Busick."
+    },
+    {
+        titulo: "Garfield: fuera de casa",
+        año: 2024,
+        imagen:"peliculas/infantil/garfield-fuera-de-casa/garfield-fuera-de-casa.webp",
+        imagen_horizontal: "https://imagenes.heraldo.es/files/image_990_556/uploads/imagenes/2023/11/14/garfield-pelicula-2024-trailer-estreno.jpeg",
+        genero: "Infantil",
+        descripcion: "El mundialmente famoso Garfield, el gato casero que odia los lunes y que adora la lasaña, está a punto de vivir una aventura ¡en el salvaje mundo exterior! Tras una inesperada reunión con su largamente perdido padre."
+    },
+    {
+        titulo: "Kung Fu Panda 4",
+        año: 2024,
+        imagen: "peliculas/infantil/kung-fu-panda-4/kung-fu-panda-4.jpg",
+        imagen_horizontal: "https://rubik-audiovisual.com/wp-content/uploads/2024/03/kung-fu-panda-4-scaled.jpg",
+        genero: "Infantil",
+        descripcion: "Po está entrenando a un nuevo guerrero que ocupe su lugar para que él pueda convertirse en líder espiritual del Valle de la Paz. Sin embargo, la irrupción de una malvada hechicera que puede cambiar de apariencia trastoca sus planes."
+    },
+    {
+        titulo: "Despicable Me 4",
+        año: 2024,
+        imagen: "peliculas/infantil/despicable-me-4/despicable-me-4.jpg",
+        imagen_horizontal: "https://cooperativa.cl/noticias/site/artic/20240129/imag/foto_0000000220240129093758.jpg",
+        genero: "Infantil",
+        descripcion: "Despicable Me 4 es una próxima película de comedia animada estadounidense producida por Illumination y distribuida por Universal Pictures.",
+    },
+];
+
+const seriesPopulares = [
+    {
+        titulo: "How to Get Away with Murder",
+        año: 2014,
+        imagen: "series/suspenso/murder/murder.jpg",
+        imagen_horizontal: "series/suspenso/murder/murder-horizontal.webp",
+        genero: "Suspenso",
+        descripcion: "Annalise Keating es una brillante y carismática profesora de derecho penal, que inesperadamente, tiene que aplicar todo el conocimiento práctico junto con sus estudiantes cuando se ven involucrados en un complot de asesinato en la universidad.",
+    },
+    {
+        titulo: "La ley de los Audaces",
+        año: 2011,
+        imagen: "series/drama-legal/suits/433386.webp",
+        imagen_horizontal: "series/drama-legal/suits/gettyimages-880283214.webp",
+        genero: "Drama Legal",
+        descripcion: "El gran abogado corporativo de Manhattan, Harvey Specter y su equipo, se lanzan a un juego por obtener el poder cuando un nuevo socio se une a la empresa. Con sus dos mejores colegas fuera y Jessica de regreso en Chicago, Specter y el equipo intentan adaptarse a una nueva situación sin ellos.",
+    },
+    {
+        titulo: "Stranger Things",
+        año: 2016,
+        imagen: "series/terror/stranger-things/5638861.webp",
+        imagen_horizontal: "series/terror/stranger-things/Stranger_Things_logo.png",
+        genero: "Terror",
+        descripcion: "Cuando un niño desaparece, sus amigos, la familia y la policía se ven envueltos en una serie de eventos misteriosos al tratar de encontrarlo. Su ausencia coincide con el avistamiento de una criatura terrorífica y la aparición de una extraña niña.",
+    },
+    {
+        titulo: "Cobra Kai",
+        año: 2018,
+        imagen: "series/drama/cobra-kai/MV5BYWU4ZmI0NTItZjcyNy00MzQ5LThiNDQtZDZkNjg1NWUwN2RhXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_.jpg",
+        imagen_horizontal: "series/drama/cobra-kai/16102988786234.webp",
+        genero: "Drama",
+        descripcion: "Treinta y cuatro años después de los eventos del torneo de karate All Valley de 1984, Johnny Lawrence busca la redención al abrir un dojo Cobra Kai, reavivando su rivalidad con un exitoso Daniel LaRusso.",
+    },
+]
+
+function crearTarjetaPelicula(pelicula) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+  
+    const imagen = document.createElement("img");
+    imagen.src = pelicula.imagen;
+    imagen.alt = pelicula.titulo;
+  
+    const contenido = document.createElement("div");
+    contenido.classList.add("card-content");
+    contenido.textContent = pelicula.titulo;
+  
+    const detalles = document.createElement("div");
+    detalles.classList.add("movie-details");
+
+    let timeoutId;
+  
+    const detallesImagen = document.createElement("img");
+    detallesImagen.src = pelicula.imagen_horizontal;
+    detallesImagen.alt = pelicula.titulo;
+    detallesImagen.style.position = "absolute";
+    detallesImagen.style.left = "0";
+    detallesImagen.style.top = "0";
+    // detallesImagen.style.width = "100%";
+    // detallesImagen.style.height = "100%";
+    detallesImagen.style.borderRadius = "20px";
+    detallesImagen.style.zIndex = "-1";
+    detallesImagen.style.overflow = "hidden%";
+    detallesImagen.style.maskImage = "linear-gradient(black 5%,transparent)";
+  
+    const detallesTitulo = document.createElement("h3");
+    detallesTitulo.textContent = pelicula.titulo;
+    detallesTitulo.style.marginTop = "110px";
+    detallesTitulo.style.padding = "0 20px";
+    
+  
+    const detallesBoton = document.createElement("button");
+    detallesBoton.textContent = "Ver ahora";
+    detallesBoton.style.fontSize = "unset"
+    detallesBoton.style.fontWeight = "bold";
+    detallesBoton.style.marginBottom = "10px";
+    detallesBoton.style.marginLeft = "20px";
+    detallesBoton.style.padding = "10px 70px";
+    detallesBoton.style.borderRadius = "30px";
+    detallesBoton.style.borderStyle = "none";
+  
+    const detallesGenero = document.createElement("p");
+    detallesGenero.textContent = `Género: ${pelicula.genero}`;
+    detallesGenero.style.marginBottom = "10px";
+    detallesGenero.style.padding = "0 20px";
+  
+    const detallesDescripcion = document.createElement("p");
+    detallesDescripcion.textContent = pelicula.descripcion;
+    detallesDescripcion.style.padding = "0 20px";
+  
+    detalles.appendChild(detallesImagen);
+    detalles.appendChild(detallesTitulo);
+    detalles.appendChild(detallesBoton);
+    detalles.appendChild(detallesGenero);
+    detalles.appendChild(detallesDescripcion);
+  
+    card.appendChild(imagen);
+    card.appendChild(contenido);
+    card.appendChild(detalles);
+  
+    card.addEventListener("mouseover", () => {
+        // Elimina cualquier temporizador pendiente
+        clearTimeout(timeoutId);
+    
+        // Establece un nuevo temporizador de 1 segundo (1000 milisegundos)
+        timeoutId = setTimeout(() => {
+          detalles.classList.add("show");
+        }, 500);
+      });
+    
+      card.addEventListener("mouseout", () => {
+        clearTimeout(timeoutId); // Elimina el temporizador
+        detalles.classList.remove("show");
+      });
+    
+      return card;
+    }
+
+// Función para mostrar las películas de recién estreno en tarjetas
+function mostrarPeliculasRecienEstrenadas() {
+    const contenedorRecienEstrenadas = document.querySelector(
+        ".card-container-recien-estrenadas"
+    );
+
+    peliculasRecienEstrenadas.forEach((peliculasTendencia) => {
+        const card = crearTarjetaPelicula(peliculasTendencia);
+        contenedorRecienEstrenadas.appendChild(card);
+    });
+}
+
+// Función para mostrar las series populares estreno en tarjetas
+function mostrarSeriesPopulares() {
+    const contenedorSeriesPopulares = document.querySelector(
+        ".card-container-series-populares"
+    );
+
+    seriesPopulares.forEach((peliculasTendencia) => {
+        const card = crearTarjetaPelicula(peliculasTendencia);
+        contenedorSeriesPopulares.appendChild(card);
+    });
+}
+
+// Función para mostrar todas las películas en tarjetas
+function mostrarTodasLasPeliculas() {
+    const contenedor = document.querySelector(".card-container");
+
+    peliculasTendencia.forEach((peliculasTendencia) => {
+        const card = crearTarjetaPelicula(peliculasTendencia);
+        contenedor.appendChild(card);
+    });
+}
+
+// Llama a las funciones para mostrar las películas cuando la página cargue
+window.onload = () => {
+    mostrarTodasLasPeliculas();
+    mostrarPeliculasRecienEstrenadas();
+    mostrarSeriesPopulares();
+};
